9a4a6dde4794b8d40372c6bc7bb9759ae755468c
DomovoiSasha <domovoi_sasha@mail.ru>
2018-06-08 03:26:09 +0500

    add libs, Portfolio, tabs and popup

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -8,4 +8,26 @@
 		$(menu).stop().slideToggle(400);
 	});
 	
+	$('#tabs').tabs({
+		event: "mouseover"
+	});
+	
+	$('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] 
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+			}
+		}
+	});
+	
 })()
