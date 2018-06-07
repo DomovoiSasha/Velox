ba295d09ec9c0154064a40616b01f1e52e10c6fd
DomovoiSasha <domovoi_sasha@mail.ru>
2018-06-07 17:46:44 +0500

    add Header and mobile menu

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,6 +1,11 @@
 (function(){
 	'use strict'
 	
+	const menuBtn = $('.top-header__btn-menu');
+	const menu = $('.main-nav');
 	
+	$(menuBtn).click(function(e){
+		$(menu).stop().slideToggle(400);
+	});
 	
 })()
